a5add8dcf7d51ea2e165d640763dc92d79071797
sasha-who <warmsasha@gmail.com>
2020-06-17 09:22:50 +0300

    Создаёт и отрендеривает компонент FilmCard

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Main from "../main/main.jsx";
 
 // eslint-disable-next-line react/prop-types
-const App = ({title, genre, date}) => {
+const App = ({title, genre, date, filmTitles}) => {
   return (
     <Main
       title={title}
       genre={genre}
       date={date}
+      filmTitles={filmTitles}
     />
   );
 };
