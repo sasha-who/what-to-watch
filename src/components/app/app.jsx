116975f942991f8da1e6078253f6129dddeb4c40
sasha-who <warmsasha@gmail.com>
2020-06-27 23:19:41 +0300

    Модифицирует App из функционального в класс-компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 
-const App = ({promoFilmData, films, onTitleClick}) => (
-  <Main
-    promoFilmData={promoFilmData}
-    films={films}
-    onTitleClick={onTitleClick}
-  />
-);
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {promoFilmData, films, onTitleClick} = this.props;
+
+    return (
+      <Main
+        promoFilmData={promoFilmData}
+        films={films}
+        onTitleClick={onTitleClick}
+      />
+    );
+  }
+}
 
 App.propTypes = {
   promoFilmData: PropTypes.shape({
