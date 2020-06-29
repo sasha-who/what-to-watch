673756eb0a4c5386948d0258ae7473fa5bfd5b93
sasha-who <warmsasha@gmail.com>
2020-06-29 10:03:39 +0300

    Дополняет моки карточки фильма информацией для вкладки Overview

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,4 +1,4 @@
-import {getRandomIntegerNumber} from "../utils/common.js";
+import {getRandomIntegerNumber, getRandomArrayItem, getRandomArrayItems} from "../utils/common.js";
 
 const IdRange = {
   MIN: 1,
@@ -28,10 +28,67 @@ const COVERS = [
   `img/johnny-english.jpg`
 ];
 
+const GENRES = [
+  `comedy`,
+  `drama`,
+  `melodrama`,
+  `horror`
+];
+
+const ReleaseRange = {
+  MIN: 1980,
+  MAX: 2020
+};
+
+const RatingRange = {
+  MIN: 0,
+  MAX: 10
+};
+
+const RatingsCountRange = {
+  MIN: 0,
+  MAX: 1000
+};
+
+const DESCRRIPTION = `In the 1930s, the Grand Budapest Hotel is a popular European ski resort,
+presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's
+friend and protege. Gustave prides himself on providing first-class service to the hotel's guests,
+including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's
+lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief
+suspect in her murder.`;
+
+const DIRECTORS = [
+  `Otto Bathurst`,
+  `Tom Harper`,
+  `Colm McCarthy`,
+  `Tim Mielants`,
+  `David Caffrey`
+];
+
+const ACTORS = [
+  `Cillian Murphy`,
+  `Sam Neill`,
+  `Helen McCrory`,
+  `Paul Anderson`,
+  `Annabelle Wallis`,
+  `Iddo Goldberg`,
+  `Sophie Rundle`,
+  `Joe Cole`,
+  `Finn Cole`
+];
+
 export const films = TITLES.map((title, index) => {
   return ({
     id: getRandomIntegerNumber(IdRange.MIN, IdRange.MAX),
     title,
-    cover: COVERS[index]
+    cover: COVERS[index],
+    poster: COVERS[index],
+    genre: getRandomArrayItem(GENRES),
+    release: getRandomIntegerNumber(ReleaseRange.MIN, ReleaseRange.MAX),
+    rating: getRandomIntegerNumber(RatingRange.MIN, RatingRange.MAX),
+    ratingsCount: getRandomIntegerNumber(RatingsCountRange.MIN, RatingsCountRange.MAX),
+    description: DESCRRIPTION,
+    director: getRandomArrayItem(DIRECTORS),
+    actors: getRandomArrayItems(ACTORS)
   });
 });
--- src/utils/common.js
+++ src/utils/common.js
@@ -4,3 +4,25 @@ export const getRandomIntegerNumber = (min, max) => {
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export const getRandomArrayItem = (elements) => {
+  const randomIndex = getRandomIntegerNumber(0, elements.length - 1);
+
+  return elements[randomIndex];
+};
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+};
+
+export const getRandomArrayItems = (array) => {
+  const shuffledArray = shuffleArray(array.slice());
+
+  return shuffledArray.slice(0, getRandomIntegerNumber(0, array.length));
+};
