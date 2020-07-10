8ad1935463e87d69bf0eb2d61782b6d7f5fa7d16
sasha-who <warmsasha@gmail.com>
2020-07-10 10:27:24 +0300

    Подключает моки для store

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,19 +1,31 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
 import {PromoFilmData, films, GENRES} from "../../test-mocks.js";
 import {App} from "./app.jsx";
+
+const mockStore = configureStore([]);
+
+const store = mockStore({
+  currentGenre: GENRES[0],
+  films,
+  filteredFilms: films
+});
 
 it(`App should render correctly`, () => {
   const tree = renderer
     .create(
-        <App
-          promoFilmData={PromoFilmData}
-          films={films}
-          filteredFilms={films}
-          currentGenre={GENRES[0]}
-          onGenreChange={() => {}}
-          filterFilmsByGenre={() => {}}
-        />, {
+        <Provider store={store}>
+          <App
+            promoFilmData={PromoFilmData}
+            films={films}
+            filteredFilms={films}
+            currentGenre={GENRES[0]}
+            onGenreChange={() => {}}
+            filterFilmsByGenre={() => {}}
+          />
+        </Provider>, {
           createNodeMock: () => {
             return {};
           }
