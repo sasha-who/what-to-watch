323b6063720d92a8d9173940bb9072215350b863
sasha-who <warmsasha@gmail.com>
2020-07-10 10:20:17 +0300

    Обновляет снепшот-тест для App, правит тест Reducer и создаёт новый для Action creator

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {PromoFilmData, films} from "../../test-mocks.js";
+import {PromoFilmData, films, GENRES} from "../../test-mocks.js";
 import {App} from "./app.jsx";
 
 it(`App should render correctly`, () => {
@@ -9,6 +9,10 @@ it(`App should render correctly`, () => {
         <App
           promoFilmData={PromoFilmData}
           films={films}
+          filteredFilms={films}
+          currentGenre={GENRES[0]}
+          onGenreChange={() => {}}
+          filterFilmsByGenre={() => {}}
         />, {
           createNodeMock: () => {
             return {};
--- src/reducer/reducer.test.js
+++ src/reducer/reducer.test.js
@@ -1,7 +1,7 @@
 import {GENRES} from "../test-mocks.js";
 import {getFilmsFilteredByGenre} from "../reducer/reducer.js";
 import {films} from "../test-mocks.js";
-import {reducer, ActionType} from "./reducer.js";
+import {reducer, ActionType, ActionCreator} from "./reducer.js";
 
 it(`Reducer should change current genre by a given value`, () => {
   expect(reducer({
@@ -30,16 +30,45 @@ it(`Reducer should change current genre by a given value`, () => {
   });
 });
 
+it(`Reducer should return all films if all films filter was chosen`, () => {
+  expect(reducer({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: films
+  }, {
+    type: ActionType.FILTER_FILMS_BY_GENRE
+  })).toEqual({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: getFilmsFilteredByGenre(films, GENRES[0])
+  });
+});
+
 it(`Reducer should return filtered films list`, () => {
   expect(reducer({
     currentGenre: GENRES[1],
     films,
     filteredFilms: films
   }, {
-    type: ActionType.GET_FILTERED_FILMS
+    type: ActionType.FILTER_FILMS_BY_GENRE
   })).toEqual({
     currentGenre: GENRES[1],
     films,
     filteredFilms: getFilmsFilteredByGenre(films, GENRES[1])
+  });
+});
+
+describe(`Action creators work correctly`, () => {
+  it(`Action creator for changing genre returns correct action`, () => {
+    expect(ActionCreator.changeCurrentGenre(GENRES[1])).toEqual({
+      type: ActionType.CHANGE_CURRENT_GENRE,
+      payload: GENRES[1]
+    });
+  });
+
+  it(`Action creator for filtering films returns correct action`, () => {
+    expect(ActionCreator.filterFilmsByGenre()).toEqual({
+      type: ActionType.FILTER_FILMS_BY_GENRE
+    });
   });
 });
