73d1fe921efa1d41e0f02602fec9b91882fbe0df
sasha-who <warmsasha@gmail.com>
2020-07-11 20:01:16 +0300

    Добавляет тест для проверки, что ShowMoreButton не рендерится, когда не нужно, и e2e-тест

--- src/components/films-list/films-list.test.js
+++ src/components/films-list/films-list.test.js
@@ -4,11 +4,30 @@ import {INITIAL_FILMS_COUNT} from "../../const.js";
 import {films} from "../../test-mocks.js";
 import FilmsList from "./films-list.jsx";
 
-it(`FilmsList should render correctly`, () => {
+it(`FilmsList should render correctly with show more button`, () => {
   const tree = renderer
     .create(
         <FilmsList
           films={films}
+          filmsCountToShow={INITIAL_FILMS_COUNT}
+          onCardClick={() => {}}
+          incrementFilmsCountToShow={() => {}}
+        />, {
+          createNodeMock: () => {
+            return {};
+          }
+        }
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it(`FilmsList should render correctly without show more button`, () => {
+  const tree = renderer
+    .create(
+        <FilmsList
+          films={films.slice(0, INITIAL_FILMS_COUNT - 1)}
           filmsCountToShow={INITIAL_FILMS_COUNT}
           onCardClick={() => {}}
           incrementFilmsCountToShow={() => {}}
--- src/reducer/reducer.test.js
+++ src/reducer/reducer.test.js
@@ -1,32 +1,36 @@
-import {GENRES} from "../test-mocks.js";
+import {INITIAL_FILMS_COUNT, ADDITIONAL_FILMS_COUNT} from "../const.js";
 import {getFilmsFilteredByGenre} from "../reducer/reducer.js";
-import {films} from "../test-mocks.js";
+import {GENRES, films} from "../test-mocks.js";
 import {reducer, ActionType, ActionCreator} from "./reducer.js";
 
 it(`Reducer should change current genre by a given value`, () => {
   expect(reducer({
     currentGenre: GENRES[0],
     films,
-    filteredFilms: films
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
   }, {
     type: ActionType.CHANGE_CURRENT_GENRE,
     payload: GENRES[1],
   })).toEqual({
     currentGenre: GENRES[1],
     films,
-    filteredFilms: films
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
   });
 
   expect(reducer({
     currentGenre: GENRES[0],
     films,
-    filteredFilms: films
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
   }, {
     type: ActionType.CHANGE_CURRENT_GENRE
   })).toEqual({
     currentGenre: GENRES[0],
     films,
-    filteredFilms: films
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
   });
 });
 
@@ -34,13 +38,15 @@ it(`Reducer should return all films if all films filter was chosen`, () => {
   expect(reducer({
     currentGenre: GENRES[0],
     films,
-    filteredFilms: films
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
   }, {
     type: ActionType.FILTER_FILMS_BY_GENRE
   })).toEqual({
     currentGenre: GENRES[0],
     films,
-    filteredFilms: getFilmsFilteredByGenre(films, GENRES[0])
+    filteredFilms: getFilmsFilteredByGenre(films, GENRES[0]),
+    filmsCountToShow: INITIAL_FILMS_COUNT
   });
 });
 
@@ -48,13 +54,63 @@ it(`Reducer should return filtered films list`, () => {
   expect(reducer({
     currentGenre: GENRES[1],
     films,
-    filteredFilms: films
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
   }, {
     type: ActionType.FILTER_FILMS_BY_GENRE
   })).toEqual({
     currentGenre: GENRES[1],
     films,
-    filteredFilms: getFilmsFilteredByGenre(films, GENRES[1])
+    filteredFilms: getFilmsFilteredByGenre(films, GENRES[1]),
+    filmsCountToShow: INITIAL_FILMS_COUNT
+  });
+});
+
+it(`Reducer should reset films count to show`, () => {
+  expect(reducer({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT + 1
+  }, {
+    type: ActionType.RESET_FILMS_COUNT_TO_SHOW
+  })).toEqual({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: getFilmsFilteredByGenre(films, GENRES[0]),
+    filmsCountToShow: INITIAL_FILMS_COUNT
+  });
+});
+
+it(`Reducer should reset films count to show`, () => {
+  expect(reducer({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT + 1
+  }, {
+    type: ActionType.RESET_FILMS_COUNT_TO_SHOW
+  })).toEqual({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
+  });
+});
+
+it(`Reducer should increment films count to show`, () => {
+  expect(reducer({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT
+  }, {
+    type: ActionType.INCREMENT_FILMS_COUNT_TO_SHOW
+  })).toEqual({
+    currentGenre: GENRES[0],
+    films,
+    filteredFilms: films,
+    filmsCountToShow: INITIAL_FILMS_COUNT + ADDITIONAL_FILMS_COUNT
   });
 });
 
