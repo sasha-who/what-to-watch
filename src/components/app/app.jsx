dd140433b6dc6ef0c9b77e700ad4cc8eded34682
sasha-who <warmsasha@gmail.com>
2020-07-12 18:11:38 +0300

    Переносит activeScreen из стейта App в стор

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,20 +8,8 @@ import Main from "../main/main.jsx";
 import DetailedFilmCard from "../detailed-card/detailed-card.jsx";
 
 class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeScreen: Screen.MAIN
-    };
-
-    this.activeFilm = null;
-    this._handlerCardClick = this._handlerCardClick.bind(this);
-  }
-
   render() {
-    const {films} = this.props;
-    const [film] = films;
+    const {films, activeFilm, onScreenChange, onActiveFilmChange} = this.props;
 
     return (
       <BrowserRouter>
@@ -31,9 +19,10 @@ class App extends React.PureComponent {
           </Route>
           <Route exact path="/film-card">
             <DetailedFilmCard
-              film={film}
+              film={activeFilm}
               films={films}
-              onCardClick={this._handlerCardClick}
+              onScreenChange={onScreenChange}
+              onActiveFilmChange={onActiveFilmChange}
             />
           </Route>
         </Switch>
@@ -41,28 +30,22 @@ class App extends React.PureComponent {
     );
   }
 
-  _handlerCardClick(film) {
-    this.setState({
-      activeScreen: Screen.CARD
-    });
-
-    this.activeFilm = film;
-  }
-
   _renderScreen() {
     const {
+      activeScreen,
+      activeFilm,
       promoFilmData,
       films,
       currentGenre,
       filteredFilms,
       filmsCountToShow,
+      onScreenChange,
+      onActiveFilmChange,
       onGenreChange,
       filterFilmsByGenre,
       resetFilmsCountToShow,
       incrementFilmsCountToShow
     } = this.props;
-
-    const {activeScreen} = this.state;
 
     switch (activeScreen) {
       case Screen.MAIN:
@@ -73,7 +56,8 @@ class App extends React.PureComponent {
             currentGenre={currentGenre}
             filteredFilms={filteredFilms}
             filmsCountToShow={filmsCountToShow}
-            onCardClick={this._handlerCardClick}
+            onScreenChange={onScreenChange}
+            onActiveFilmChange={onActiveFilmChange}
             onGenreChange={onGenreChange}
             filterFilmsByGenre={filterFilmsByGenre}
             resetFilmsCountToShow={resetFilmsCountToShow}
@@ -84,9 +68,10 @@ class App extends React.PureComponent {
       case Screen.CARD:
         return (
           <DetailedFilmCard
-            film={this.activeFilm}
+            film={activeFilm}
             films={films}
-            onCardClick={this._handlerCardClick}
+            onScreenChange={onScreenChange}
+            onActiveFilmChange={onActiveFilmChange}
           />
         );
 
@@ -154,8 +139,35 @@ App.propTypes = {
         ).isRequired
       })
   ).isRequired,
+  activeScreen: PropTypes.string.isRequired,
+  activeFilm: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    cover: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    preview: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    release: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    ratingsCount: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    actors: PropTypes.arrayOf(PropTypes.string),
+    runTime: PropTypes.number.isRequired,
+    reviews: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          text: PropTypes.string.isRequired,
+          rating: PropTypes.number.isRequired,
+          userName: PropTypes.string.isRequired,
+          date: PropTypes.instanceOf(Date).isRequired
+        })
+    ).isRequired
+  }).isRequired,
   currentGenre: PropTypes.string.isRequired,
   filmsCountToShow: PropTypes.number,
+  onScreenChange: PropTypes.func.isRequired,
+  onActiveFilmChange: PropTypes.func.isRequired,
   onGenreChange: PropTypes.func.isRequired,
   filterFilmsByGenre: PropTypes.func.isRequired,
   resetFilmsCountToShow: PropTypes.func.isRequired,
@@ -163,6 +175,8 @@ App.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
+  activeScreen: state.activeScreen,
+  activeFilm: state.activeFilm || state.films[0],
   currentGenre: state.currentGenre,
   films: state.films,
   filteredFilms: state.filteredFilms || [],
@@ -170,6 +184,12 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  onScreenChange(screen) {
+    dispatch(ActionCreator.changeActiveScreen(screen));
+  },
+  onActiveFilmChange(film) {
+    dispatch(ActionCreator.setActiveFilm(film));
+  },
   onGenreChange(genre) {
     dispatch(ActionCreator.changeCurrentGenre(genre));
   },
