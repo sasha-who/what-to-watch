b98aa7df422b6d9bd543ff48bf731eebf6020811
sasha-who <warmsasha@gmail.com>
2020-07-22 10:23:28 +0300

    Добавляет тесты для компонента withPlayer

--- src/components/player/player.test.js
+++ src/components/player/player.test.js
@@ -1,11 +1,24 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {films} from "../../test-mocks.js";
 import Player from "../player/player.jsx";
 
 it(`Player should render correctly`, () => {
-  const tree = renderer
-    .create(<Player />)
-    .toJSON();
+  const tree = renderer.create(
+      <Player
+        film={films[0]}
+        isPlaying={true}
+        onPlayerStateChange={() => {}}
+        progress={0}
+        onPlayButtonClick={() => {}}
+        onFullScreenButtonClick={() => {}}
+      >
+        <video />
+      </Player>, {
+        createNodeMock: () => {
+          return {};
+        }
+      }).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
