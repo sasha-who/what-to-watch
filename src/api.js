004e29a8bc6f8d7cdc3f658a822bf9781489cfab
sasha-who <warmsasha@gmail.com>
2020-07-23 22:22:23 +0300

    Добавляет модуль для работы с API

---
+++ src/api.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {UNAUTHORIZED_ERROR, SERVER_TIMEOUT, BASE_URL} from "./const.js";
+
+export const createAPI = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: SERVER_TIMEOUT,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === UNAUTHORIZED_ERROR) {
+      onUnauthorized();
+
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
