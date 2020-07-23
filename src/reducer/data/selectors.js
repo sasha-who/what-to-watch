86f709d778d4f81bd1dd6825f3055ad3f62d7d7e
sasha-who <warmsasha@gmail.com>
2020-07-23 22:02:44 +0300

    Добавляет селекторы и вносит мелкие правки

---
+++ src/reducer/data/selectors.js
@@ -0,0 +1,11 @@
+import NameSpace from "../name-space.js";
+
+const NAME_SPACE = NameSpace.DATA;
+
+export const getFilms = (state) => {
+  return state[NAME_SPACE].films;
+};
+
+export const getPromoFilm = (state) => {
+  return state[NAME_SPACE].promoFilm;
+};
