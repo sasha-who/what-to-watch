56efb8a3588d4e9af6658a88e850f8cfedc4eed0
sasha-who <warmsasha@gmail.com>
2020-07-24 09:51:18 +0300

    Подключает thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 import App from "./components/app/app.jsx";
 import reducer from "./reducer/reducer.js";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as AppStateOperation} from "./reducer/app-state/app-state.js";
+import {createAPI} from "./api";
+
+const api = createAPI(() => {});
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
 
 const rootElement = document.querySelector(`#root`);
