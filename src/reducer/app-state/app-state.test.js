dfdd4315fc07e745b768fb1a75b574af112142e0
sasha-who <warmsasha@gmail.com>
2020-07-24 09:36:34 +0300

    Удаляет лишние тесты

--- src/reducer/app-state/app-state.test.js
+++ src/reducer/app-state/app-state.test.js
@@ -1,6 +1,5 @@
 import {INITIAL_FILMS_COUNT, ADDITIONAL_FILMS_COUNT, Screen} from "../../const.js";
-import {getFilmsFilteredByGenre} from "../../utils/common.js";
-import {GENRES, films} from "../../test-mocks.js";
+import {GENRES} from "../../test-mocks.js";
 import {reducer, ActionType, ActionCreator} from "./app-state.js";
 
 it(`Reducer should change current genre by a given value`, () => {
@@ -33,46 +32,6 @@ it(`Reducer should change current genre by a given value`, () => {
     activeScreen: Screen.MAIN,
     currentGenre: GENRES[0],
     filteredFilms: [],
-    filmsCountToShow: INITIAL_FILMS_COUNT,
-    isPlayerActive: false
-  });
-});
-
-it(`Reducer should return all films if all films filter was chosen`, () => {
-  expect(reducer({
-    activeScreen: Screen.MAIN,
-    currentGenre: GENRES[0],
-    filteredFilms: [],
-    filmsCountToShow: INITIAL_FILMS_COUNT,
-    isPlayerActive: false
-  }, {
-    type: ActionType.FILTER_FILMS_BY_GENRE,
-    payload: films
-  })).toEqual({
-    activeScreen: Screen.MAIN,
-    currentGenre: GENRES[0],
-    filteredFilms: films,
-    filmsCountToShow: INITIAL_FILMS_COUNT,
-    isPlayerActive: false
-  });
-});
-
-it(`Reducer should return filtered films list`, () => {
-  const filteredFilms = getFilmsFilteredByGenre(films, GENRES[1]);
-
-  expect(reducer({
-    activeScreen: Screen.MAIN,
-    currentGenre: GENRES[1],
-    filteredFilms: [],
-    filmsCountToShow: INITIAL_FILMS_COUNT,
-    isPlayerActive: false
-  }, {
-    type: ActionType.FILTER_FILMS_BY_GENRE,
-    payload: filteredFilms
-  })).toEqual({
-    activeScreen: Screen.MAIN,
-    currentGenre: GENRES[1],
-    filteredFilms,
     filmsCountToShow: INITIAL_FILMS_COUNT,
     isPlayerActive: false
   });
@@ -114,28 +73,6 @@ it(`Reducer should increment films count to show`, () => {
   });
 });
 
-// it(`Reducer should set similar films`, () => {
-//   const similarFilms = getSimilarFilms(films, films[0]);
-
-//   expect(reducer({
-//     activeScreen: Screen.MAIN,
-//     currentGenre: GENRES[0],
-//     filteredFilms: [],
-//     filmsCountToShow: INITIAL_FILMS_COUNT,
-//     isPlayerActive: false
-//   }, {
-//     type: ActionType.SET_SIMILAR_FILMS,
-//     payload: similarFilms
-//   })).toEqual({
-//     activeScreen: Screen.MAIN,
-//     currentGenre: GENRES[0],
-//     filteredFilms: [],
-//     filmsCountToShow: INITIAL_FILMS_COUNT,
-//     similarFilms,
-//     isPlayerActive: false
-//   });
-// });
-
 it(`Reducer should change player state`, () => {
   expect(reducer({
     activeScreen: Screen.MAIN,
@@ -162,13 +99,6 @@ describe(`Action creators work correctly`, () => {
     });
   });
 
-  it(`Action creator for filtering films returns correct action`, () => {
-    expect(ActionCreator.filterFilmsByGenre(films)).toEqual({
-      type: ActionType.FILTER_FILMS_BY_GENRE,
-      payload: films
-    });
-  });
-
   it(`Action creator for reseting films count to show returns correct action`, () => {
     expect(ActionCreator.resetFilmsCountToShow()).toEqual({
       type: ActionType.RESET_FILMS_COUNT_TO_SHOW
@@ -178,13 +108,6 @@ describe(`Action creators work correctly`, () => {
   it(`Action creator for incrementing films count to show returns correct action`, () => {
     expect(ActionCreator.incrementFilmsCountToShow()).toEqual({
       type: ActionType.INCREMENT_FILMS_COUNT_TO_SHOW
-    });
-  });
-
-  it(`Action creator for setting similar films returns correct action`, () => {
-    expect(ActionCreator.setSimilarFilms(films)).toEqual({
-      type: ActionType.SET_SIMILAR_FILMS,
-      payload: films
     });
   });
 
