c050bfe808a546b9192d58abb6baef44ee8ec526
sasha-who <warmsasha@gmail.com>
2020-07-28 10:03:38 +0300

    Добавляет и обновляет тесты

--- src/reducer/user/user.test.js
+++ src/reducer/user/user.test.js
@@ -1,48 +1,70 @@
 import {AuthorizationStatus} from "../../const.js";
 import {reducer, ActionCreator, ActionType} from "./user.js";
-
+import {userData} from "../../test-mocks.js";
 
 it(`Reducer without additional parameters should return initial state`, () => {
   expect(reducer(void 0, {})).toEqual({
     authorizationStatus: AuthorizationStatus.NO_AUTHORIZED,
+    authorizationData: {}
   });
 });
 
 it(`Reducer should change authorizationStatus by a given value`, () => {
   expect(reducer({
     authorizationStatus: AuthorizationStatus.NO_AUTHORIZED,
+    authorizationData: {}
   }, {
     type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: AuthorizationStatus.AUTHORIZED,
+    payload: AuthorizationStatus.AUTHORIZED
   })).toEqual({
     authorizationStatus: AuthorizationStatus.AUTHORIZED,
+    authorizationData: {}
   });
 
   expect(reducer({
     authorizationStatus: AuthorizationStatus.AUTHORIZED,
+    authorizationData: {}
   }, {
     type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: AuthorizationStatus.NO_AUTHORIZED,
+    payload: AuthorizationStatus.NO_AUTHORIZED
   })).toEqual({
     authorizationStatus: AuthorizationStatus.NO_AUTHORIZED,
+    authorizationData: {}
   });
 
   expect(reducer({
     authorizationStatus: AuthorizationStatus.AUTHORIZED,
+    authorizationData: {}
   }, {
     type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: AuthorizationStatus.AUTHORIZED,
+    payload: AuthorizationStatus.AUTHORIZED
   })).toEqual({
     authorizationStatus: AuthorizationStatus.AUTHORIZED,
+    authorizationData: {}
   });
 
   expect(reducer({
     authorizationStatus: AuthorizationStatus.NO_AUTHORIZED,
+    authorizationData: {}
   }, {
     type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: AuthorizationStatus.NO_AUTHORIZED,
+    payload: AuthorizationStatus.NO_AUTHORIZED
   })).toEqual({
     authorizationStatus: AuthorizationStatus.NO_AUTHORIZED,
+    authorizationData: {}
+  });
+});
+
+it(`Reducer should set authorizationData`, () => {
+  expect(reducer({
+    authorizationStatus: AuthorizationStatus.AUTHORIZED,
+    authorizationData: {}
+  }, {
+    type: ActionType.GET_USER_DATA,
+    payload: userData
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.AUTHORIZED,
+    authorizationData: userData
   });
 });
 
@@ -56,6 +78,13 @@ describe(`Action creators work correctly`, () => {
     expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTHORIZED)).toEqual({
       type: ActionType.REQUIRED_AUTHORIZATION,
       payload: AuthorizationStatus.AUTHORIZED,
+    });
+  });
+
+  it(`Action creator for getting user data returns correct action`, () => {
+    expect(ActionCreator.getUserData(userData)).toEqual({
+      type: ActionType.GET_USER_DATA,
+      payload: userData,
     });
   });
 });
