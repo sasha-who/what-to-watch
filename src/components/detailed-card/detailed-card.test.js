84156378a75e95bf457e68da6cddf61294d8ec2e
sasha-who <warmsasha@gmail.com>
2020-07-28 09:53:35 +0300

    Реализует логику отрисовки компонента Header в зависимости от статуса авторизации

--- src/components/detailed-card/detailed-card.test.js
+++ src/components/detailed-card/detailed-card.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {films} from "../../test-mocks.js";
+import {AuthorizationStatus} from "../../const.js";
+import {films, useData} from "../../test-mocks.js";
 import DetailedFilmCard from "../detailed-card/detailed-card.jsx";
 
 const [film] = films;
@@ -9,6 +10,8 @@ it(`DetailedFilmCard should render correctly`, () => {
   const tree = renderer
     .create(
         <DetailedFilmCard
+          authorizationStatus={AuthorizationStatus.NO_AUTHORIZED}
+          authorizationData={useData}
           film={film}
           similarFilms={films}
           isPlayerActive={false}
--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,10 +1,17 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {AuthorizationStatus} from "../../const.js";
+import {userData} from "../../test-mocks.js";
 import Header from "./header.jsx";
 
 it(`Header should render correctly`, () => {
   const tree = renderer
-    .create(<Header />)
+    .create(
+        <Header
+          authorizationStatus={AuthorizationStatus.AUTHORIZED}
+          authorizationData={userData}
+        />
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
